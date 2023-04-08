f035d01e398162276c42dbc15129e73c7e92dad6
Nyasaa <114298259+Nyasaa@users.noreply.github.com>
2023-04-08 18:55:31 +0530

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -5,26 +5,7 @@ const bodyParser = require("body-parser");
 const app = express();
  
 app.use(bodyParser.urlencoded({ extended: true}));
- 
- 
-app.get("/", (req, res) => {
-    
-    res.sendFile(__dirname + "/index.html");
- 
-});
- 
-app.post("/", (req, res) => {
-    
-    var num1 = Number(req.body.num1);
-    var num2 = Number(req.body.num2);
- 
-    var result = num1 + num2;
- 
- 
-    res.send("The Result of The Calculation is " + result);
-});
- 
- 
+
  
 app.get("/bmiCalculator", (req, res) => {
     
